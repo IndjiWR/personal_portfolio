5d197e3881e6eddeaa8d28f9b09a3abf7bd61c95
indji00 <147103631+indji00@users.noreply.github.com>
2026-04-05 10:27:48 +0200

    Auto-generated commit

---
+++ chunk-I2DIRDDH.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e,f}from"./chunk-GXUBQFXJ.js";import"./chunk-KYU4YV6R.js";export{a as PROJECT_ENVIRONMENT,c as ProjectCard,f as ProjectDetail,d as ProjectSection,b as ProjectService,e as Projects};
